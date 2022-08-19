d77ab77bc2957e4165201f68f54b258103212fdd
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-08-19 14:38:43 +0300

    refactor: add routing to App

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,8 +1,24 @@
+import { Route, Routes } from 'react-router-dom';
+import { Main } from './pages/Main';
+import { Textbook } from './pages/Textbook';
+import { Audiocall } from './pages/Audiocall';
+import { Sprint } from './pages/Sprint';
+import { Statistics } from './pages/Statistics';
+import { Login } from './pages/Login';
+import { Registration } from './pages/Registration';
+
 function App() {
+	document.body.classList.add('scroll');
 	return (
-		<div>
-			App
-		</div>
+		<Routes>
+			<Route path="/" element={<Main />} />
+			<Route path="/textbook" element={<Textbook />} />
+			<Route path="/audiocall" element={<Audiocall />} />
+			<Route path="/sprint" element={<Sprint />} />
+			<Route path="/statistics" element={<Statistics />} />
+			<Route path="/login" element={<Login />} />
+			<Route path="/registration" element={<Registration />} />
+		</Routes>
 	);
 }
 
