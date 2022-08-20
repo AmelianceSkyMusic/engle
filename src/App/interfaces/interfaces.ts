27d6b99fff2c307def64a892cf11646e930763c2
DStukalo <94228608+DStukalo@users.noreply.github.com>
2022-08-20 14:45:04 +0300

    refactor: changed comma to semicolon at interfaces, the options object is described

--- src/App/interfaces/interfaces.ts
+++ src/App/interfaces/interfaces.ts
@@ -1,46 +1,51 @@
 export interface IWord {
-    id: string,
-    group: number,
-    page: number,
-    word: string,
-    image: string,
-    audio: string,
-    audioMeaning: string,
-    audioExample: string,
-    textMeaning: string,
-    textExample: string,
-    transcription: string,
-    wordTranslate: string,
-    textMeaningTranslate: string,
-    textExampleTranslate: string
+    id: string;
+    group: number;
+    page: number;
+    word: string;
+    image: string;
+    audio: string;
+    audioMeaning: string;
+    audioExample: string;
+    textMeaning: string;
+    textExample: string;
+    transcription: string;
+    wordTranslate: string;
+    textMeaningTranslate: string;
+    textExampleTranslate: string;
 }
 
 export interface IAuthentication {
-    message:	string,
-    token:	string,
-    refreshToken:	string,
-    userId:	string,
-    name:	string
+    message:	string;
+    token:	string;
+    refreshToken:	string;
+    userId:	string;
+    name:	string;
 }
 
 export interface IUser {
-    name:	string,
-    email:	string,
-    password:	string
+    name:	string;
+    email:	string;
+    password:	string;
 }
 
 export interface IUserWord {
-    difficulty:	string,
-    optional: {}
+    difficulty:	string;
+    optional: {
+        [key: string]: unknown;
+    };
 }
 
-
 export interface IStatistic {
-    learnedWords: number,
-    optional: {}
+    learnedWords: number;
+    optional: {
+        [key: string]: unknown;
+    };
 }
 
 export interface ISetting {
-    wordsPerDay: number,
-    optional: {}
+    wordsPerDay: number;
+    optional: {
+        [key: string]: unknown;
+    };
 }
