67c78ebbfc6e608ff907321549c91e7a832951c0
DStukalo <94228608+DStukalo@users.noreply.github.com>
2022-08-20 13:19:08 +0300

    feat: add interface to request unautoriz user get words

---
+++ src/App/interfaces/interfaces.ts
@@ -0,0 +1,19 @@
+export interface IWord {
+    id: string,
+    group: number,
+    page: number,
+    word: string,
+    image: string,
+    audio: string,
+    audioMeaning: string,
+    audioExample: string,
+    textMeaning: string,
+    textExample: string,
+    transcription: string,
+    wordTranslate: string,
+    textMeaningTranslate: string,
+    textExampleTranslate: string
+}
+
+
+
