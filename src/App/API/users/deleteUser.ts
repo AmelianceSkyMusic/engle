68724e020b6ace1dff4a1a4c028d421588266c5c
DStukalo <94228608+DStukalo@users.noreply.github.com>
2022-08-23 16:56:37 +0300

    feat: request to delete user

---
+++ src/App/API/users/deleteUser.ts
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+import { BASE_URL } from '../Base_url';
+
+export const deleteUser = async (userId: string, token: string) => {
+	const res = await axios({
+		method: 'delete',
+		url: `${BASE_URL}users/${userId}`,
+		headers: {
+			Accept: 'application/json',
+			'Content-Type': 'application/json',
+			Authorization: `Bearer ${token}`,
+		},
+	});
+	return res;
+};
