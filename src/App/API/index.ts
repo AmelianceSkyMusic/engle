fe605bbd82550d3b3e111f28c79322a871d9111a
DStukalo <94228608+DStukalo@users.noreply.github.com>
2022-08-23 17:05:32 +0300

    feat: create index where import\export functions for working with the server

---
+++ src/App/API/index.ts
@@ -0,0 +1,53 @@
+import { getWords } from './words/getWords';
+import { getWordsByID } from './words/getWordByID';
+
+import { signIn } from './login/signIn';
+
+import { createNewUser } from './users/createNewUser';
+import { getUser } from './users/getUser';
+import { updateUser } from './users/updateUser';
+import { deleteUser } from './users/deleteUser';
+import { getNewUserToken } from './users/getNewUserToken';
+
+import { createUserWord } from './users/words/createUserWord';
+import { getUserWords } from './users/words/getUserWords';
+import { getUserWordByID } from './users/words/getUserWordByID';
+import { updateUserWord } from './users/words/updateUserWord';
+import { deleteUserWord } from './users/words/deleteUserWord';
+
+import { getUserSettings } from './users/settings/getUserSettings';
+import { updateUserSettings } from './users/settings/updateUserSettings';
+
+import { getUserStatistics } from './users/statistic/getUserStatistic';
+import { updateUserStatistics } from './users/statistic/updateUserStatistic';
+
+import { getUserAggregateWords } from './users/aggregatedWords/getUserAggregateWords';
+import { getUserAggregateWordsByID } from './users/aggregatedWords/getUserAggregateWordsByID';
+
+export default {
+	getWords,
+	getWordsByID,
+
+	signIn,
+
+	createNewUser,
+	getUser,
+	updateUser,
+	deleteUser,
+	getNewUserToken,
+	getUserWords,
+
+	createUserWord,
+	getUserWordByID,
+	updateUserWord,
+	deleteUserWord,
+
+	getUserSettings,
+	updateUserSettings,
+
+	getUserStatistics,
+	updateUserStatistics,
+
+	getUserAggregateWords,
+	getUserAggregateWordsByID,
+};
