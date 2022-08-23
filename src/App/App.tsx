462ab4458c005a198cedbcde7ee446ac008855e4
DStukalo <94228608+DStukalo@users.noreply.github.com>
2022-08-23 16:50:31 +0300

    refactor: add import separation

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
+
 import { Main } from './pages/Main';
 import { Textbook } from './pages/Textbook';
 import { Audiocall } from './pages/Audiocall';
