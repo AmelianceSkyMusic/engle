b21a2bb8e7926f97f2554f890eaee2f825559ef0
DStukalo <94228608+DStukalo@users.noreply.github.com>
2022-08-24 21:08:01 +0300

    refactor: add type to return and change return of request login

--- src/App/API/login/signIn.ts
+++ src/App/API/login/signIn.ts
@@ -1,13 +1,14 @@
 import axios from 'axios';
 
 import { BASE_URL } from '../consts';
+import { IAuthentication } from '../../interfaces/interfaces';
 
-export const signIn = async (mail: string, pass: string) => {
+export const signIn = async (mail: string, pass: string):Promise<IAuthentication> => {
 	const res = await axios.post(`${BASE_URL}signin`, {
 		email: `${mail}`,
 		password: `${pass}`,
 	});
 	sessionStorage.setItem('refreshToken', `${res.data.refreshToken}`);
 	sessionStorage.setItem('token', `${res.data.token}`);
-	return res;
+	return res.data;
 };
