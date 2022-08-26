a2a96a6f83fc69c24a596959642249aa1be933a8
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-08-26 10:18:32 +0300

    refactor: add page loader component

---
+++ src/App/components/Loader.tsx
@@ -0,0 +1,12 @@
+export function Loader() {
+	return (
+		<div className="zero-block">
+			<div className="blackout show" />
+			<div className="page-loader">
+				<div />
+				<div />
+				<div />
+			</div>
+		</div>
+	);
+}
