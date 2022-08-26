0142ad62756cb49da1991c0c2f9adec23fe3e68b
DStukalo <94228608+DStukalo@users.noreply.github.com>
2022-08-26 14:24:13 +0300

    refactor: update return and type return of the function create ner user

--- src/App/API/users/createNewUser.ts
+++ src/App/API/users/createNewUser.ts
@@ -3,11 +3,16 @@ import axios from 'axios';
 import { BASE_URL } from '../consts';
 import { IUser } from '../../interfaces/interfaces';
 
-export const createNewUser = async (name:string, mail: string, pass: string):Promise<IUser> => {
+type response = {
+	data: IUser;
+	statusText: string;
+}
+
+export const createNewUser = async (name:string, mail: string, pass: string):Promise<response> => {
 	const res = await axios.post(`${BASE_URL}users`, {
 		name: `${name}`,
 		email: `${mail}`,
 		password: `${pass}`,
 	});
-	return res.data;
+	return { data: res.data, statusText: res.statusText };
 };
