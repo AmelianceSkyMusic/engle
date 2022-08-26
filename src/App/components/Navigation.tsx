ccc0071e13bb6e243ec87818de2c52ad180e4d4a
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-08-26 21:26:26 +0300

    refactor: move theme button

--- src/App/components/Navigation.tsx
+++ src/App/components/Navigation.tsx
@@ -1,5 +1,5 @@
 import { Link, useNavigate } from 'react-router-dom';
-import { ThemeButton } from './ThemeButton';
+import { ThemeButton } from '../../asmlib/asm-scripts/components/ThemeButton';
 
 export interface NavigationProps {
 	logoOnly?: boolean;
