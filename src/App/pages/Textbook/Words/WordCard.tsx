f6359e2837c5b0bb77c81a4d270a9aca2743a113
Maksym Kantor <max.kantor72@gmail.com>
2022-08-27 17:58:21 +0300

    feat: add popups for word cards

--- src/App/pages/Textbook/Words/WordCard.tsx
+++ src/App/pages/Textbook/Words/WordCard.tsx
@@ -1,11 +1,13 @@
+import { useState } from 'react';
 import { IWord } from '../../../types/interfaces';
 import { BASE_URL } from '../../../API/consts';
+import { Modal } from '../../../../asmlib/asm-ui/components/Modal';
 
 interface WordCardProps {
   word: IWord;
 }
-
 export function WordCard({ word }: WordCardProps) {
+	const [modal, setModal] = useState(false);
 	const imgUrl = `${BASE_URL}${word.image}`;
 	return (
 		<li className="word-card">
@@ -21,7 +23,58 @@ export function WordCard({ word }: WordCardProps) {
 				</div>
 				<div className="word-card__panel-row">
 					<div className="word-card__label" />
-					<button type="button" className="icon click icon--corner-down-right">{}</button>
+					<button type="button" className="icon click icon--corner-down-right" onClick={() => setModal(true)}>
+						{}
+					</button>
+					{modal && (
+						<Modal setOpen={setModal}>
+							<div className="word-modal">
+								<div className="word-modal__column col-7 col-md-12">
+									<img src={imgUrl} alt={word.word} className="word-modal__image" />
+									<div className="word-modal__statistic">
+										<h4 className="h4">Статистика</h4>
+										<p className="word-modal__correct-answers p1">
+											Угадано: 8
+										</p>
+										<p className="word-modal__incorrect-answers p1">
+											Не угадано: 10
+										</p>
+									</div>
+									<div className="word-modal__controls">
+										<button type="button" className="word-modal__add-to-hard button-sm secondary">
+											Добавить в сложные
+										</button>
+										<button type="button" className="word-modal__add-to-learned button-sm secondary">
+											Добавить в изученные
+										</button>
+									</div>
+								</div>
+								<div className="word-modal__column col-5 col-md-12">
+									<div className="word-modal__main-info">
+										<h3 className="word-modal__word h3">{word.word}</h3>
+										<p className="word-modal__translation p1">{word.wordTranslate}</p>
+										<p className="word-modal__transcription p1">{word.transcription}</p>
+									</div>
+									<div className="word-modal__explanations">
+										<h4 className="h4">Значение</h4>
+										<p
+											className="word-modal__meaning p1"
+											dangerouslySetInnerHTML={{ __html: word.textMeaning }}
+										/>
+										<p className="word-modal__meaning-translation p2">{word.textMeaningTranslate}</p>
+									</div>
+									<div className="word-modal__examples">
+										<h4 className="h4">Пример</h4>
+										<p
+											className="word-modal__example p1"
+											dangerouslySetInnerHTML={{ __html: word.textExample }}
+										/>
+										<p className="word-modal__example-translation p2">{word.textExampleTranslate}</p>
+									</div>
+								</div>
+							</div>
+						</Modal>
+					)}
 				</div>
 			</div>
 		</li>
