ef00a8785304be039c90501aed5034d2f15ec0ad
DStukalo <94228608+DStukalo@users.noreply.github.com>
2022-09-02 16:08:15 +0300

    feat: add getUserStatistic

--- src/App/API/users/statistic/axios/getUserStatistic.ts
+++ src/App/API/users/statistic/axios/getUserStatistic.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-import { BASE_URL } from '../../consts';
-import { IStatistic } from '../../../types/interfaces';
+import { BASE_URL } from '../../../consts';
+import { IStatistic } from '../../../../types/interfaces';
 
 export const getUserStatistics = async (userId: string): Promise<IStatistic> => {
 	const token = `${localStorage.getItem('token')}`;
@@ -12,5 +12,6 @@ export const getUserStatistics = async (userId: string): Promise<IStatistic> => 
 			'Content-Type': 'application/json',
 		},
 	});
+	console.log(res);
 	return res.data;
 };
