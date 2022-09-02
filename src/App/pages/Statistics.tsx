d1701cd9425a10d8e8bf3d9a49b1379908a8b84b
DStukalo <94228608+DStukalo@users.noreply.github.com>
2022-09-02 16:46:09 +0300

    refactor: update statistic

--- src/App/pages/Statistics.tsx
+++ src/App/pages/Statistics.tsx
@@ -2,6 +2,16 @@ import '../../styles/pages/statistics.scss';
 
 import { Footer } from '../components/layouts/Footer';
 import { Header } from '../components/layouts/Header';
+// import { getUserStatistics } from '../API/users/statistic/axios/getUserStatistic';
+// import { updateUserStatistics } from '../API/users/statistic/axios/updateUserStatistic';
+import { updateStatistic } from '../API/users/statistic/updateStatistics';
+
+const { userId } = JSON.parse(localStorage.getItem('user') as string);
+console.log(localStorage.getItem('user'));
+// updateStatistic(userId, 'textbook', 'countNewWords', [15]);
+// updateStatistic(userId, 'textbook', 'learnedWords', ['5e9f5ee35eb9e72bc21af4f3']);
+// updateStatistic(userId, 'textbook', 'newWords', ['5e9f5ee35eb9e72bc21af4f3']);
+updateStatistic(userId, 'sprint', 'countNewWords', [11]);
 
 export function Statistics() {
 	return (
