8c59ee6b65d10ab5de782f6b525970797a37629f
DStukalo <94228608+DStukalo@users.noreply.github.com>
2022-09-03 22:17:54 +0300

    refactor: update axios func to API

--- src/App/API/users/words/getUserWordByID.ts
+++ src/App/API/users/words/getUserWordByID.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 import { BASE_URL } from '../../consts';
-import { IUserWord } from '../../../types/interfaces';
+import { IUserWord, IUserWordWithId } from '../../../types/interfaces';
 
-export const getUserWordByID = async (userId: string, wordId: string): Promise<IUserWord> => {
-	const token = `${sessionStorage.getItem('token')}`;
+export const getUserWordByID = async (userId: string, wordId: string): Promise<IUserWordWithId> => {
+	const token = `${localStorage.getItem('token')}`;
 	const res = await axios.get(`${BASE_URL}users/${userId}/words/${wordId}`, {
 		headers: {
 			Authorization: `Bearer ${token}`,
--- src/App/API/users/words/getUserWords.ts
+++ src/App/API/users/words/getUserWords.ts
@@ -4,7 +4,7 @@ import { BASE_URL } from '../../consts';
 import { IUserWord } from '../../../types/interfaces';
 
 export const getUserWords = async (userId: string): Promise<IUserWord[]> => {
-	const token = `${sessionStorage.getItem('token')}`;
+	const token = `${localStorage.getItem('token')}`;
 	const res = await axios.get(`${BASE_URL}users/${userId}/words`, {
 		headers: {
 			Authorization: `Bearer ${token}`,
