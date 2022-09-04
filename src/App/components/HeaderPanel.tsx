806dbbb05c4d85986c5534536bf5aac2496e08a4
Maksym Kantor <max.kantor72@gmail.com>
2022-09-05 01:51:39 +0300

    fix: reset currentGroup when logging out in hardWords

--- src/App/components/HeaderPanel.tsx
+++ src/App/components/HeaderPanel.tsx
@@ -31,6 +31,9 @@ export function HeaderPanel({ isHideRegistration }: IHeaderPanelProps) {
 		localStorage.removeItem('refreshToken');
 		localStorage.removeItem('token');
 		dispatch(initUserAction());
+		if (localStorage.getItem('currentGroup') === '6') {
+			localStorage.setItem('currentGroup', '0');
+		}
 		window.location.reload();
 	}
 
