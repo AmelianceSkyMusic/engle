8962e6b19b7032eab8e6dd1074d139c1c2195ec3
Maksym Kantor <max.kantor72@gmail.com>
2022-09-06 01:04:30 +0300

    feat: add markup for sprint game

--- src/App/pages/games/Sprint/Game.tsx
+++ src/App/pages/games/Sprint/Game.tsx
@@ -5,19 +5,32 @@ interface IGameProps {
 }
 
 export function Game({ words }: IGameProps) {
-	console.log(words);
 	return (
-		<>
-			<div>
-				<h1 className="h1">
-					{words[0].group}
-				</h1>
+		<div className="sprint-game row">
+			<div className="sprint-game__col sprint-game__col_left col-4">
+				<div className="sprint-game__check sprint-game__check_1  icon icon--check sprint-game__row-1" />
+				<p className="sprint-game__curr-points p1 sprint-game__row-2">10 очков</p>
+				<h3 className="sprint-game__eng-word h3 sprint-game__row-3">Pasta</h3>
+				<button type="button" className="sprint-game__wrong-btn button sprint-game__row-4">
+					<span className="icon icon--arrow-left" />
+					Неверно
+				</button>
 			</div>
-			<div>
-				<h1 className="h1">
-					{words[0].page}
-				</h1>
+			<div className="sprint-game__col sprint-game__col_center col-4">
+				<div className="sprint-game__check sprint-game__check_2 icon icon--check sprint-game__row-1" />
+				<p className="sprint-game__coff p1 sprint-game__row-2">x2</p>
+				<h3 className="sprint-game__question-mark h3 sprint-game__row-3">?</h3>
+				<h4 className="sprint-game__counter h4 sprint-game__row-4">60</h4>
 			</div>
-		</>
+			<div className="sprint-game__col sprint-game__col_right col-4">
+				<div className="sprint-game__check sprint-game__check_3 icon icon--check sprint-game__row-1" />
+				<p className="sprint-game__overall-points p1 sprint-game__row-2">Всего очков: 20</p>
+				<h3 className="sprint-game__ru-word h3 sprint-game__row-3">Лололаоала</h3>
+				<button type="button" className="sprint-game__correct-btn button sprint-game__row-4">
+					Верно
+					<span className="icon icon--arrow-right" />
+				</button>
+			</div>
+		</div>
 	);
 }
