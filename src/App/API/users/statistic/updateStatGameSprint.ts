b26d21794f7f2ea17895702e2aba66d4244fb967
DStukalo <94228608+DStukalo@users.noreply.github.com>
2022-09-05 19:48:28 +0300

    feat: create func to update user statististic at section sprint game

--- src/App/API/users/statistic/updateStatGameSprint.ts
+++ src/App/API/users/statistic/updateStatGameSprint.ts
@@ -1,77 +1,46 @@
+/* eslint-disable no-param-reassign */
 import { updateUserStatistics } from './axios/updateUserStatistic';
-import { getUserStatistics } from './axios/getUserStatistic';
-import { ISprint } from '../../../types/interfaces';
+import { IStatistic } from '../../../types/interfaces';
 
-// type TOptional = Pick<IStatistic, 'optional'>;
-type TSprintGame = Partial<ISprint>
-
-async function updateStatGameSprint(userId: string, caseOfOpt: string, value: string[] | number[]) {
-	const curStat = await getUserStatistics(userId);
-	(delete curStat.id);
-	if (!('sprint' in curStat.optional)) {
-		(curStat.optional.sprint as TSprintGame) = {
-			newWords: {},
-			countNewWords: 0,
-			countShowedWords: 0,
-			countRight: 0,
-			countWrong: 0,
-			topRight: 0,
-		};
-	}
-	switch (caseOfOpt) {
-	case 'newWords':
-		if (curStat) {
+async function updateStatGameSprint(userId: string, value: object, curStat: IStatistic) {
+	Object.entries(value).forEach((el: [string, string[] | number]) => {
+		switch (el[0]) {
+		case 'newWords':
 			// eslint-disable-next-line no-case-declarations
 			const dateNow = new Date().toLocaleDateString();
-			console.log(curStat);
-			if ((dateNow in curStat.optional.sprint.newWords)) {
-				console.log('update');
-				// eslint-disable-next-line max-len
-				curStat.optional.sprint.newWords[dateNow] = [...curStat.optional.sprint.newWords[dateNow], ...value as string[]];
+			if ('newWords' in curStat.optional.sprint) {
+				if ((dateNow in curStat.optional.sprint.newWords)) {
+					// eslint-disable-next-line max-len
+					curStat.optional.sprint.newWords[dateNow] =	[...curStat.optional.sprint.newWords[dateNow], ...el[1] as string[]];
+				} else {
+					curStat.optional.sprint.newWords[dateNow] = el[1] as string[];
+				}
 			} else {
-				console.log('const');
-				curStat.optional.sprint.newWords = { [dateNow]: value as string[] };
+				curStat.optional.sprint.newWords = {
+					[dateNow]: el[1] as string[],
+				};
 			}
-			await updateUserStatistics(userId, curStat);
-		}
-		break;
-	case 'countNewWords':
-		// curStat.optional.textBook.countNewWords = Number(value[0]);
-
-		// eslint-disable-next-line @typescript-eslint/no-unused-vars, no-case-declarations
-		const newObj = {
-			countNewWord: value[0],
-		};
-		if (curStat) {
-			if ('countNewWords' in curStat.optional.sprint) {
-				curStat.optional.sprint.countNewWords = value[0] as number;
-			} else {
-				// curStat.optional.sprint = { countNewWords: value[0] as number };
-				curStat.optional.sprint.countNewWords = value[0] as number;
+			break;
+		case 'countNewWords':
+			curStat.optional.sprint.countNewWords += el[1] as number;
+			break;
+		case 'countRight':
+			curStat.optional.sprint.countRight += el[1] as number;
+			break;
+		case 'countWrong':
+			curStat.optional.sprint.countWrong += el[1] as number;
+			break;
+		case 'topRight':
+			if (curStat.optional.sprint.topRight < el[1]) {
+				curStat.optional.sprint.topRight = el[1] as number;
 			}
-			console.log(curStat);
-			await updateUserStatistics(userId, curStat);
+			break;
+		default:
+			console.log('unknown option');
+			break;
 		}
-		break;
-	case 'countRight':
-		if (curStat) {
-			await updateUserStatistics(userId, curStat);
-		}
-		break;
-	case 'countWrong':
-		if (curStat) {
-			await updateUserStatistics(userId, curStat);
-		}
-		break;
-	case 'topRight':
-		if (curStat) {
-			await updateUserStatistics(userId, curStat);
-		}
-		break;
-	default:
-		console.log('unknown option');
-		break;
-	}
+	});
+	await updateUserStatistics(userId, curStat);
 }
 
 export { updateStatGameSprint };
