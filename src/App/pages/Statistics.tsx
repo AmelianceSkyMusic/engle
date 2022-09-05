d4d61920a6fda68344b46aac164b91c6922b8f69
DStukalo <94228608+DStukalo@users.noreply.github.com>
2022-09-05 22:09:51 +0300

    refactor: start new branch

--- src/App/pages/Statistics.tsx
+++ src/App/pages/Statistics.tsx
@@ -2,8 +2,11 @@ import '../../styles/pages/statistics.scss';
 
 import { Footer } from '../components/layouts/Footer';
 import { Header } from '../components/layouts/Header';
+import API from '../API';
+import { store } from '../store';
 
 export function Statistics() {
+	const { isLogged } = store.getState().user;
 	return (
 		<div className="page-container page-statistics">
 			<Header />
