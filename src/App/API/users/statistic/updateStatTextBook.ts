115d54354b38e2587090915c1db294e80ed2507c
DStukalo <94228608+DStukalo@users.noreply.github.com>
2022-09-05 19:48:46 +0300

    feat: create func to update user statististic at section textbook

--- src/App/API/users/statistic/updateStatTextBook.ts
+++ src/App/API/users/statistic/updateStatTextBook.ts
@@ -1,43 +1,33 @@
+/* eslint-disable no-param-reassign */
 import { updateUserStatistics } from './axios/updateUserStatistic';
 import { getUserStatistics } from './axios/getUserStatistic';
 import { IStatistic } from '../../../types/interfaces';
 
-interface IStatplusID extends IStatistic {
-id: string;
-}
-
-async function updateStatTextBook(userId: string, caseOfOpt: string, value: string[] | number[]) {
-	console.log(userId);
-	// const { userId } = JSON.parse(localStorage.getItem('user') as string);
-	const curStat = await getUserStatistics(userId);
-	console.log(delete curStat.id);
-	switch (caseOfOpt) {
-	case 'learnedWords':
-		if (curStat) {
-		// eslint-disable-next-line no-case-declarations
-		// eslint-disable-next-line no-case-declarations
-			const dateNow = new Date().toLocaleDateString();
-			console.log(curStat);
-			// console.log(Object.hasOwn(curStat, dateNow));
-			if ((dateNow in curStat.optional.textBook.learnedWords)) {
-				console.log('update');
-				// eslint-disable-next-line max-len
-				curStat.optional.textBook.learnedWords[dateNow] = [...curStat.optional.textBook.learnedWords[dateNow], ...value as string[]];
+async function updateStatTextBook(
+	userId: string,
+	value: { learnedWords: string[] },
+	curStat: IStatistic,
+) {
+	const updateValue = value.learnedWords;
+	// eslint-disable-next-line no-case-declarations
+	const dateNow = new Date().toLocaleDateString();
+	if ('textBook' in curStat.optional) {
+		if (!(dateNow in curStat.optional.textBook.learnedWords)) {
+			curStat.optional.textBook.learnedWords = { [dateNow]: updateValue };
+			curStat.learnedWords += updateValue.length;
+		} else {
 			// eslint-disable-next-line max-len
-			// curStat.optional.textBook.learnedWords[dateNow] = ((curStat.optional.textBook.learnedWords[dateNow]).concat(...value as string[]));
-			} else {
-				console.log('const');
-				curStat.optional.textBook.learnedWords = { [dateNow]: value as string[] };
-			}
-			// curStat.optional.textBook.learnedWords = { [dateNow]: value as string[] };
-			// console.log(curStat);
-			await updateUserStatistics(userId, curStat);
+			curStat.optional.textBook.learnedWords[dateNow]	= ((curStat.optional.textBook.learnedWords[dateNow]).concat(...updateValue));
+			curStat.learnedWords += updateValue.length;
 		}
-		break;
-	default:
-		console.log('unknown option');
-		break;
+	} else {
+		curStat.optional.textBook = {
+			learnedWords: {
+				[dateNow]: [...updateValue],
+			},
+		};
 	}
+	await updateUserStatistics(userId, curStat);
 }
 
 export { updateStatTextBook };
