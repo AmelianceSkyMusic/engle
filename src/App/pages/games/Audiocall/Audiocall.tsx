f34dbde7beae332ec384a206841bea007ef11b09
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-09-05 18:58:49 +0300

    refactor: add start code to Audiocall

--- src/App/pages/games/Audiocall/Audiocall.tsx
+++ src/App/pages/games/Audiocall/Audiocall.tsx
@@ -6,6 +6,11 @@ import { Footer } from '../../../components/layouts/Footer';
 import { Header } from '../../../components/layouts/Header';
 import { Game } from './Game';
 import { StartScreen } from '../StartScreen';
+import { useTypedSelector } from '../../../store/hooks/useTypedSelector';
+import { IUserPageWord } from '../../../types/interfaces';
+import { getWordsAction } from '../../../store/reducers/words/actions/getWordsAction';
+import { useTypedDispatch } from '../../../store/hooks/useTypedDispatch';
+import { Loader } from '../../../../asmlib/asm-ui/components/Loader';
 
 interface IAudiocallProps {
 	groupNumber?: number;
@@ -13,43 +18,58 @@ interface IAudiocallProps {
 }
 
 export function Audiocall({ groupNumber = -1, pageNumber = -1 }: IAudiocallProps) {
+	const dispatch = useTypedDispatch();
 
+	const { userPageWords, isLoading, error } = useTypedSelector((state) => state.words);
 	const [startGameParam, setStartGameParam] = useState({ groupNumber, pageNumber });
 	const [isStartGame, setIsStartGame] = useState(false);
+	const [words, setWords] = useState<IUserPageWord[]>(userPageWords);
 
 	useEffect(() => {
+
+		console.log(startGameParam);
 		if (startGameParam.groupNumber >= 0 && startGameParam.pageNumber >= 0) {
+			dispatch(getWordsAction(startGameParam.groupNumber, startGameParam.pageNumber));
+		}
+	}, [dispatch, startGameParam]);
+
+	useEffect(() => {
+		setWords(userPageWords);
+		if (words.length > 0) {
 			setIsStartGame(true);
 		}
-	}, [startGameParam]);
+	}, [userPageWords, words]);
 
 	return (
 		<div className="page-container page-audiocall start-screen">
 			<Header />
 			<main className="main">
 				<div className="container row">
-					{isStartGame
-						? (
-							<Game
-								groupNumber={startGameParam.groupNumber}
-								pageNumber={startGameParam.pageNumber}
-							/>
-						)
-						: (
-							<StartScreen
-								setStartGameParam={setStartGameParam}
-								groupNumber={groupNumber}
-								pageNumber={pageNumber}
-							>
-								<>
-									<h1 className="title__heading h1">Игра «Аудиовызов»</h1>
-									<p className="title__description p1">
-										Тренировка слуха.
-										<br />
-										Прослушайте слово и выберите правильный вариант
-									</p>
-								</>
-							</StartScreen>
+					{ /* eslint-disable-next-line no-nested-ternary */ }
+					{ isLoading
+						? <Loader />
+						: (isStartGame
+							? (
+								<Game
+									words={words}
+								/>
+							)
+							: (
+								<StartScreen
+									setStartGameParam={setStartGameParam}
+									groupNumber={groupNumber}
+									pageNumber={pageNumber}
+								>
+									<>
+										<h1 className="title__heading h1">Игра «Аудиовызов»</h1>
+										<p className="title__description p1">
+											Тренировка слуха.
+											<br />
+											Прослушайте слово и выберите правильный вариант
+										</p>
+									</>
+								</StartScreen>
+							)
 						)}
 				</div>
 			</main>
