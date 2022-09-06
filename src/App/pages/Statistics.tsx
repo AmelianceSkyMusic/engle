ef4e8feed6782ad0811595e9669ed15c2645373e
DStukalo <94228608+DStukalo@users.noreply.github.com>
2022-09-06 06:41:09 +0300

    refactor: update statistics.tsx

--- src/App/pages/Statistics.tsx
+++ src/App/pages/Statistics.tsx
@@ -1,20 +1,59 @@
+import { useState, useEffect } from 'react';
+
 import '../../styles/pages/statistics.scss';
 
 import { Footer } from '../components/layouts/Footer';
 import { Header } from '../components/layouts/Header';
 import API from '../API';
 import { store } from '../store';
+import { IStatistic } from '../types/interfaces';
+import { userCreateStatMarkup } from './Statistics/UserStatistic';
 
 export function Statistics() {
-	const { isLogged } = store.getState().user;
+	const { isLogged, userId, userName } = store.getState().user;
+
+	const [stateShort, setStateShort] = useState({});
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await API.getUserStatistics(userId);
+				setStateShort(response);
+			} catch (error) {
+				console.error(error);
+			}
+		};
+		fetchData();
+
+	}, [userId]);\
+
 	return (
 		<div className="page-container page-statistics">
 			<Header />
 			<main className="main">
 				<div className="container row">
-					<h1 className="h1">
-						Statistics
-					</h1>
+					{isLogged
+						? (
+							<div className="page-statistics__header-block">
+								<h2 className="page-statistics__heading h2">
+									Cтатистика
+								</h2>
+								<h3 className="page-statistics__heading h3">
+									{userName}
+								</h3>
+							</div>
+						)
+						: 	(
+							<div className="page-statistics__header-block">
+								<h2 className="page-statistics__heading h2">
+									Cтатистика
+								</h2>
+								<h3 className="page-statistics__heading h3">Войдите чтоб увидеть статистику</h3>
+							</div>
+						)}
+					{ stateShort && isLogged && (
+						userCreateStatMarkup(stateShort as IStatistic)
+					)}
 				</div>
 			</main>
 			<Footer />
