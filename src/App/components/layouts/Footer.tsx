fc6ff9627746b98593b54a36cc580a3d68447c4c
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-09-06 08:12:09 +0300

    refactor: add some fixes

--- src/App/components/layouts/Footer.tsx
+++ src/App/components/layouts/Footer.tsx
@@ -6,9 +6,9 @@ export function Footer() {
 					<a className="link inverted github-logo" target="_blank" href="https://github.com/" rel="noreferrer">{}</a>
 				</div>
 				<div className="footer__names col-6 col-xs-12">
-					<a className="link inverted" target="_blank" href="https://dribbble.com/" rel="noreferrer">Dima</a>
-					<a className="link inverted" target="_blank" href="https://dribbble.com/" rel="noreferrer">Max</a>
-					<a className="link inverted" target="_blank" href="https://dribbble.com/" rel="noreferrer">AmelianceSkyMusic</a>
+					<a className="link inverted" target="_blank" href="https://github.com/DStukalo/" rel="noreferrer">Dima</a>
+					<a className="link inverted" target="_blank" href="https://github.com/Kantor17/" rel="noreferrer">Max</a>
+					<a className="link inverted" target="_blank" href="https://github.com/AmelianceSkyMusic/" rel="noreferrer">AmelianceSkyMusic</a>
 				</div>
 				<div className="footer__rsschool-logo col-3 col-xs-6">
 					<a className="link inverted rsschool-logo" target="_blank" href="https://rs.school/js/" rel="noreferrer">{}</a>
