b0d731de8db7e9c10c76efe30de9b057d1f1376b
DStukalo <94228608+DStukalo@users.noreply.github.com>
2022-12-23 12:55:26 +0200

    refactor: change page statistics, add links to navigate for nested routes

--- src/App/pages/Statistics.tsx
+++ src/App/pages/Statistics.tsx
@@ -1,15 +1,16 @@
 import { useState, useEffect } from 'react';
+import {	Outlet, Link, useLocation } from 'react-router-dom';
 
-import '../../styles/pages/statistics.scss';
-
-import { Footer } from '../components/layouts/Footer';
-import { Header } from '../components/layouts/Header';
 import API from '../API';
 import { store } from '../store';
+import { Footer } from '../components/layouts/Footer';
+import { Header } from '../components/layouts/Header';
+import { Loader } from '../../asmlib/asm-ui/components/Loader';
 import { IStatistic } from '../types/interfaces';
-import { userCreateStatMarkup } from './Statistics/UserStatistic';
+import '../../styles/pages/statistics.scss';
 
 export function Statistics() {
+	const location = useLocation();
 	const { isLogged, userId, userName } = store.getState().user;
 
 	const [stateShort, setStateShort] = useState<IStatistic>();
@@ -26,38 +27,68 @@ export function Statistics() {
 			}
 		};
 		fetchData();
-
 	}, [userId]);
+
+	function isActive(pathStat: string) {
+		if (location.pathname.includes(pathStat)) return 'page-statistics__links-item-link_active';
+		return 'page-statistics__links-item-link';
+	}
 
 	return (
 		<div className="page-container page-statistics">
 			<Header />
 			<main className="main">
 				<div className="container row">
-					{isLogged
-						? (
-							<div className="page-statistics__header-block">
-								<h2 className="page-statistics__heading h2">
-									Статистика
-								</h2>
-								<h3 className="page-statistics__heading h3">
-									{userName}
-								</h3>
-							</div>
-						)
-						: 	(
-							<div className="page-statistics__header-block">
-								<h2 className="page-statistics__heading h2">
-									Статистика
-								</h2>
-								<h3 className="page-statistics__heading h3">Войдите чтоб увидеть статистику</h3>
-							</div>
-						)}
-					{ stateShort && isLogged && !loading && (
-						userCreateStatMarkup(stateShort)
-					)}
-					{ !stateShort && isLogged && (
-						<h4 className="page-statistics__heading h4">Вы пока не проводили действий, отслеживаемых статистикой</h4>
+					{loading ? (
+						<Loader />
+					) : (
+						<>
+							{isLogged ? (
+								<div className="page-statistics__header-block">
+									<h2 className="page-statistics__heading h2">
+										Статистика
+									</h2>
+									<h3 className="page-statistics__heading h3">
+										{userName}
+									</h3>
+								</div>
+							) : (
+								<div className="page-statistics__header-block">
+									<h2 className="page-statistics__heading h2">
+										Статистика
+									</h2>
+									<h3 className="page-statistics__heading h3">
+										Войдите чтоб увидеть статистику
+									</h3>
+								</div>
+							)}
+							{stateShort
+								&& isLogged
+								&& !loading
+									&& (
+										<>
+											<h4 className="h4">Ми собрали немного твоей статистики, выбирай какую увидеть:</h4>
+											<ul className="page-statistics__links-list">
+												<li className="page-statistics__links-item h4">
+													<Link to="long" className={isActive('long')}>Долгосрочная статистика</Link>
+												</li>
+												<li className="page-statistics__links-item h4">
+													<Link to="simple" className={isActive('simple')}>Простая статистика</Link>
+												</li>
+											</ul>
+										</>
+									)}
+							{stateShort
+								&& isLogged
+								&& !loading
+									&& <Outlet context={stateShort} />}
+							{!stateShort && isLogged && (
+								<h4 className="page-statistics__heading h4">
+									Вы пока не проводили действий, отслеживаемых
+									статистикой
+								</h4>
+							)}
+						</>
 					)}
 				</div>
 			</main>
