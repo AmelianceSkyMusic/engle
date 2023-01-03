67e50308932f16d9f3c4d3347d8da9e4c6e8eab4
Maksym Kantor <max.kantor72@gmail.com>
2023-01-03 12:01:42 +0200

    fix: stop timer when there is loading

--- src/App/pages/games/Sprint/Timer.tsx
+++ src/App/pages/games/Sprint/Timer.tsx
@@ -1,20 +1,23 @@
 import { useState, useEffect } from 'react';
+import { useTypedSelector } from '../../../store/hooks/useTypedSelector';
 
 interface ITimerProps {
   onEndCallback: () => void;
 }
 export function Timer({ onEndCallback }: ITimerProps) {
 	const [seconds, setSeconds] = useState(60);
+	const { isLoading } = useTypedSelector((state) => state.words);
+
 	useEffect((): undefined | (() => void) => {
 		if (seconds >= 1) {
 			const interval = setInterval(() => {
-				setSeconds(seconds - 1);
+				if (!isLoading) setSeconds(seconds - 1);
 			}, 1000);
 			return () => clearInterval(interval);
 		}
 		onEndCallback();
 		return undefined;
-	}, [seconds, onEndCallback]);
+	}, [seconds, onEndCallback, isLoading]);
 	return (
 		<h4 className="sprint-game__counter h4 sprint-game__row-4">{seconds}</h4>
 	);
